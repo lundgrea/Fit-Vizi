4b64450591c661930ef0f663a268afafb0d9125a
Alyssa Lundgren <lundgrea@gmail.com>
2019-12-14 18:20:57 -0700

    Revise apikey to fix greyed out map issue

--- src/components/MapDisplay/MapDisplay.js
+++ src/components/MapDisplay/MapDisplay.js
@@ -1,25 +1,29 @@
 import React from 'react'
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+const API_KEY =`${process.env.REACT_APP_API_KEY}`
 
 export const MapDisplay = (props) => {
-
     return (
+      <>
         <Map
           google={props.google}
           zoom={3}
           style={mapStyles}
           initialCenter={{ lat: 47.444, lng: -122.176}}
         />
+        <Marker position={{ lat: 48.00, lng: -122.00}} />
+      </>
     );
 } 
 
 
 export default GoogleApiWrapper({
-  apiKey: process.env_REACT_APP_API_KEY
+  apiKey: API_KEY
 })(MapDisplay);
 
 
 const mapStyles = {
-  width: '100%',
-  height: '100%',
+  width: '40%',
+  height: '40%',
 };
