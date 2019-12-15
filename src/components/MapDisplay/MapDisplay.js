1e5aade4c725f79b082c75f26336467ba9780963
Alyssa Lundgren <lundgrea@gmail.com>
2019-12-15 16:46:19 -0700

    Complete polyline example

--- src/components/MapDisplay/MapDisplay.js
+++ src/components/MapDisplay/MapDisplay.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import React, { Component } from 'react'
+import { Map, GoogleApiWrapper, Polyline } from 'google-maps-react';
 
 const API_KEY =`${process.env.REACT_APP_API_KEY}`
 
-export const MapDisplay = (props) => {
+
+const triangleCoords = [
+  {lat: 47.774, lng: -122.180},
+  {lat: 48.466, lng: -123.118},
+  {lat: 46.321, lng: -124.757},
+  {lat: 47.774, lng: -122.190}
+];
+
+export class MapDisplay extends Component {
+render() {
     return (
       <>
         <Map
-          google={props.google}
-          zoom={3}
+          google={this.props.google}
+          zoom={7}
           style={mapStyles}
           initialCenter={{ lat: 47.444, lng: -122.176}}
-        />
-        <Marker position={{ lat: 48.00, lng: -122.00}} />
+        >
+          <Polyline
+          path={triangleCoords}
+          strokeColor="#0000FF"
+          strokeOpacity={0.8}
+          strokeWeight={2} />
+        </Map>
       </>
     );
 } 
-
+}
 
 export default GoogleApiWrapper({
   apiKey: API_KEY
