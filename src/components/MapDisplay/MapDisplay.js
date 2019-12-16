1071606d50f031a8ccce82200932dc462133b3fd
Alyssa Lundgren <lundgrea@gmail.com>
2019-12-15 18:20:27 -0700

    Reduce data to polyline on map

--- src/components/MapDisplay/MapDisplay.js
+++ src/components/MapDisplay/MapDisplay.js
@@ -1,35 +1,66 @@
 import React, { Component } from 'react'
 import { Map, GoogleApiWrapper, Polyline } from 'google-maps-react';
+import { workoutData } from '../../workout-data';
 
 const API_KEY =`${process.env.REACT_APP_API_KEY}`
 
 
-const triangleCoords = [
-  {lat: 47.774, lng: -122.180},
-  {lat: 48.466, lng: -123.118},
-  {lat: 46.321, lng: -124.757},
-  {lat: 47.774, lng: -122.190}
-];
+// const triangleCoords = [
+//   {lat: 47.774, lng: -122.180},
+//   {lat: 48.466, lng: -123.118},
+//   {lat: 46.321, lng: -124.757},
+//   {lat: 47.774, lng: -122.190}
+// ];
+
+let routes 
 
 export class MapDisplay extends Component {
-render() {
+
+ 
+  displayPolyline = () => {
+    let routes = this.generatePolylines()
+    return (
+      <Polyline
+          path={routes}
+          strokeColor="#0000FF"
+          strokeOpacity={0.8}
+          strokeWeight={2} />
+        
+    )
+  }
+
+
+  generatePolylines = () => {
+    let samples = workoutData.samples
+    let paths = samples.reduce((endValue, sample) => {
+      if (sample.values.positionLat != undefined) {
+        let object = {lat: '', lng: ''}
+        object.lat = sample.values.positionLat
+        object.lng = sample.values.positionLong
+        endValue.push(object)
+      return endValue
+      } else {
+      return endValue
+      }
+    }, [{lat: 40.01488, lng: -105.131}])
+    return paths
+  }
+
+
+  render() {
     return (
       <>
         <Map
           google={this.props.google}
-          zoom={7}
+          zoom={15}
           style={mapStyles}
-          initialCenter={{ lat: 47.444, lng: -122.176}}
+          initialCenter={{ lat: 40.01488, lng: -105.131}}
         >
-          <Polyline
-          path={triangleCoords}
-          strokeColor="#0000FF"
-          strokeOpacity={0.8}
-          strokeWeight={2} />
+          {this.displayPolyline()}
         </Map>
       </>
     );
-} 
+  } 
 }
 
 export default GoogleApiWrapper({
@@ -38,6 +69,6 @@ export default GoogleApiWrapper({
 
 
 const mapStyles = {
-  width: '40%',
-  height: '40%',
+  width: '100%',
+  height: '100%',
 };
