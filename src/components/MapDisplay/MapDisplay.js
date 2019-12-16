8bd3d339d901eb73b24cc8539249c4f995c31334
Alyssa Lundgren <lundgrea@gmail.com>
2019-12-15 18:23:25 -0700

    Change color of polyline

--- src/components/MapDisplay/MapDisplay.js
+++ src/components/MapDisplay/MapDisplay.js
@@ -5,12 +5,6 @@ import { workoutData } from '../../workout-data';
 const API_KEY =`${process.env.REACT_APP_API_KEY}`
 
 
-// const triangleCoords = [
-//   {lat: 47.774, lng: -122.180},
-//   {lat: 48.466, lng: -123.118},
-//   {lat: 46.321, lng: -124.757},
-//   {lat: 47.774, lng: -122.190}
-// ];
 
 let routes 
 
@@ -21,11 +15,11 @@ export class MapDisplay extends Component {
     let routes = this.generatePolylines()
     return (
       <Polyline
-          path={routes}
-          strokeColor="#0000FF"
-          strokeOpacity={0.8}
-          strokeWeight={2} />
-        
+        path={routes}
+        strokeColor="#DB3A34"
+        strokeOpacity={0.8}
+        strokeWeight={5} 
+      />
     )
   }
 
