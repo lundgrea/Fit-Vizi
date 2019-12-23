7a2d853bf212d27e55cbc7b4f16380951ee323ca
Alyssa Lundgren <lundgrea@gmail.com>
2019-12-23 16:24:43 -0700

    Add start index and duration to best display

--- src/components/BestDisplay/BestDisplay.js
+++ src/components/BestDisplay/BestDisplay.js
@@ -5,11 +5,16 @@ export const BestDisplay = (props) => {
   return (
     <>
     <h2>Best Average Power Outputs</h2>
-    <p>1 Minute: {props.data.topOne.averagePower}</p>
-    <p>5 Minutes: {props.data.topFive.averagePower}</p>
-    <p>10 Minutes: {props.data.topTen.averagePower}</p>
-    <p>15 Minutes: {props.data.topFifteen.averagePower}</p>
-    <p>20 Minutes: {props.data.topTwenty.averagePower}</p>
+    <h4>1 Minute: {props.data.topOne.averagePower}</h4>
+    <p>Record Began {props.data.topOne.startIndex} Seconds Into and Lasted 60 Seconds </p>
+    <h4>5 Minutes: {props.data.topFive.averagePower}</h4>
+    <p>Record Began {props.data.topFive.startIndex} Seconds Into and Lasted 5 Minutes</p>
+    <h4>10 Minutes: {props.data.topTen.averagePower}</h4>
+    <p>Record Began {props.data.topTen.startIndex} Seconds Into and Lasted 10 Minutes</p>
+    <h4>15 Minutes: {props.data.topFifteen.averagePower}</h4>
+    <p>Record Began {props.data.topFifteen.startIndex} Seconds Into and Lasted 15 Minutes</p>
+    <h4>20 Minutes: {props.data.topTwenty.averagePower}</h4>
+    <p>Record Began {props.data.topTwenty.startIndex} Seconds Into and Lasted 20 Minutes</p>
     </>
   )
 }
