1cf67c8e40191318b7a2e0598bbd9f2f3ffe088e
Alyssa Lundgren <lundgrea@gmail.com>
2019-12-23 16:25:32 -0700

    Remove console log and correct indentation in BestDisplay

--- src/components/BestDisplay/BestDisplay.js
+++ src/components/BestDisplay/BestDisplay.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
 export const BestDisplay = (props) => {
-  console.log(props)
   return (
     <>
-    <h2>Best Average Power Outputs</h2>
-    <h4>1 Minute: {props.data.topOne.averagePower}</h4>
-    <p>Record Began {props.data.topOne.startIndex} Seconds Into and Lasted 60 Seconds </p>
-    <h4>5 Minutes: {props.data.topFive.averagePower}</h4>
-    <p>Record Began {props.data.topFive.startIndex} Seconds Into and Lasted 5 Minutes</p>
-    <h4>10 Minutes: {props.data.topTen.averagePower}</h4>
-    <p>Record Began {props.data.topTen.startIndex} Seconds Into and Lasted 10 Minutes</p>
-    <h4>15 Minutes: {props.data.topFifteen.averagePower}</h4>
-    <p>Record Began {props.data.topFifteen.startIndex} Seconds Into and Lasted 15 Minutes</p>
-    <h4>20 Minutes: {props.data.topTwenty.averagePower}</h4>
-    <p>Record Began {props.data.topTwenty.startIndex} Seconds Into and Lasted 20 Minutes</p>
+      <h2>Best Average Power Outputs</h2>
+      <h4>1 Minute: {props.data.topOne.averagePower}</h4>
+      <p>Record Began {props.data.topOne.startIndex} Seconds Into and Lasted 60 Seconds </p>
+      <h4>5 Minutes: {props.data.topFive.averagePower}</h4>
+      <p>Record Began {props.data.topFive.startIndex} Seconds Into and Lasted 5 Minutes</p>
+      <h4>10 Minutes: {props.data.topTen.averagePower}</h4>
+      <p>Record Began {props.data.topTen.startIndex} Seconds Into and Lasted 10 Minutes</p>
+      <h4>15 Minutes: {props.data.topFifteen.averagePower}</h4>
+      <p>Record Began {props.data.topFifteen.startIndex} Seconds Into and Lasted 15 Minutes</p>
+      <h4>20 Minutes: {props.data.topTwenty.averagePower}</h4>
+      <p>Record Began {props.data.topTwenty.startIndex} Seconds Into and Lasted 20 Minutes</p>
     </>
   )
 }
